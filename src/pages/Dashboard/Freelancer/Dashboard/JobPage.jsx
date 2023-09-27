e74cd74071d42ddf942cfabbd1043ed10d5825fe
Olosko23 <oloogeorge633@gmail.com>
2023-09-27 10:41:41 +0300

    'place_bids'

--- src/pages/Dashboard/Freelancer/Dashboard/JobPage.jsx
+++ src/pages/Dashboard/Freelancer/Dashboard/JobPage.jsx
@@ -46,7 +46,6 @@ const JobPage = () => {
 
     try {
       const formData = new FormData();
-      formData.append("job_id", jobId);
       formData.append("price", bidAmount);
       formData.append("proposal", proposal);
       for (const file of files) {
@@ -54,7 +53,7 @@ const JobPage = () => {
       }
 
       const response = await axios.post(
-        "https://assist-api-okgk.onrender.com/api/v1/place-bid",
+        `https://assist-api-okgk.onrender.com/api/v1/place-bid/${jobId}`,
         formData,
         {
           headers: {
@@ -78,14 +77,14 @@ const JobPage = () => {
   };
 
   return (
-    <div className="p-4 mt-14">
+    <div className="p-4 mt-14 max-w-5xl mx-auto">
       <span
         className="underline font-semibold cursor-pointer py-2 my-2"
         onClick={() => navigate("/dashboard")}
       >
         Go Back
       </span>
-      <h2 className="text-2xl font-semibold mb-4">Job Details</h2>
+      <h2 className="text-2xl font-semibold mb-4 py-2">Project Details</h2>
       {isLoading ? (
         <p>Loading...</p>
       ) : (
@@ -93,49 +92,90 @@ const JobPage = () => {
           <h3 className="text-lg font-semibold">{job.title}</h3>
           <p className="text-gray-600">{job.description}</p>
           <p className="text-gray-600">Budget: {job.budget}</p>
-          <p className="text-gray-600">Bids: {job.bids}</p>
+          <p className="text-gray-600">Bids: {job.bids.length}</p>
           <p className="text-gray-600">Duration: {job.duration} days</p>
-          <p className="text-gray-600">Skills: {job.skills}</p>
-          <p className="text-gray-600">Files: {job.files}</p>
-          <form onSubmit={handleBidSubmit} className="mt-4">
+          <p className="text-gray-600">Skills: {job.skills.join(", ")}</p>
+          <p className="text-gray-600">Files: {job.files.length}</p>
+          <span>Click to download</span>
+          <ul>
+            {job.files.map((file, index) => (
+              <li key={index} className="hover:underline">
+                <a
+                  href={`https://assist-api-okgk.onrender.com/${file.fileUrl}`}
+                  target="_blank"
+                  rel="noreferrer"
+                  download
+                >
+                  {file.title}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+
+      <hr />
+
+      <div className="bg-white p-4 border border-gray-300 rounded-lg mt-4">
+        <h3 className="text-lg font-semibold mb-2">Place Your Bid</h3>
+        <form onSubmit={handleBidSubmit}>
+          <div className="grid grid-cols-1 gap-4">
             <div className="flex items-center">
+              <label htmlFor="bidAmount" className="text-gray-600">
+                Bid Amount
+              </label>
               <input
                 type="number"
+                id="bidAmount"
+                name="bidAmount"
                 min="0"
                 step="10"
                 placeholder="Enter your bid amount"
                 value={bidAmount}
                 onChange={(e) => setBidAmount(e.target.value)}
-                className="w-40 p-2 border border-gray-300 rounded-md mr-2"
+                className="w-full p-2 border border-gray-300 rounded-md mt-2"
                 required
               />
-              <input
-                type="text"
+            </div>
+            <div className="flex items-center">
+              <label htmlFor="proposal" className="text-gray-600">
+                Bid Proposal
+              </label>
+              <textarea
+                id="proposal"
+                name="proposal"
                 placeholder="Enter your bid proposal"
                 value={proposal}
                 onChange={(e) => setProposal(e.target.value)}
-                className="w-40 p-2 border border-gray-300 rounded-md mr-2"
+                className="w-full p-2 border border-gray-300 rounded-md mt-2"
                 required
               />
+            </div>
+            <div className="flex items-center">
+              <label htmlFor="files" className="text-gray-600">
+                Attach Files
+              </label>
               <input
                 type="file"
+                id="files"
+                name="files"
                 multiple
                 onChange={handleFileChange}
-                className="mr-2"
+                className="w-full p-2 border border-gray-300 rounded-md mt-2"
               />
-              <button
-                type="submit"
-                className={`bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600 ${
-                  isBidding ? "cursor-not-allowed opacity-50" : ""
-                }`}
-                disabled={isBidding}
-              >
-                {isBidding ? "Placing Bid..." : "Place Bid"}
-              </button>
             </div>
-          </form>
-        </div>
-      )}
+            <button
+              type="submit"
+              className={`bg-blue-500 text-white p-2 rounded-md mt-4 w-40 hover:bg-blue-600 ${
+                isBidding ? "cursor-not-allowed opacity-50" : ""
+              }`}
+              disabled={isBidding}
+            >
+              {isBidding ? "Placing Bid..." : "Place Bid"}
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
