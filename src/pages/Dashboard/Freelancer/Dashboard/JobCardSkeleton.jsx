3f6334491a63b3d83a37a4ab2cf174c2b1bca2b2
Olosko23 <oloogeorge633@gmail.com>
2023-09-29 16:12:32 +0300

    'cleanup_4'

---
+++ src/pages/Dashboard/Freelancer/Dashboard/JobCardSkeleton.jsx
@@ -0,0 +1,43 @@
+const JobCardSkeleton = () => {
+  return (
+    <>
+      <div className="bg-blue-100 p-4 mb-2 border border-blue-400 rounded-lg shadow-md">
+        <div className="animate-pulse h-6 w-1/2 mb-2 bg-blue-200 rounded-lg" />
+        <div className="animate-pulse h-6 w-1/4 mb-2 bg-blue-200 rounded-lg" />
+        <div className="animate-pulse h-4 w-1/2 mb-2 bg-blue-200 rounded-lg" />
+        <div className="animate-pulse h-4 w-3/4 mb-2 bg-blue-200 rounded-lg" />
+        <div className="animate-pulse h-4 w-2/3 bg-blue-200 rounded-lg" />
+      </div>
+      <div className="bg-blue-100 p-4 mb-2 border border-blue-400 rounded-lg shadow-md">
+        <div className="animate-pulse h-6 w-1/2 mb-2 bg-blue-200 rounded-lg" />
+        <div className="animate-pulse h-6 w-1/4 mb-2 bg-blue-200 rounded-lg" />
+        <div className="animate-pulse h-4 w-1/2 mb-2 bg-blue-200 rounded-lg" />
+        <div className="animate-pulse h-4 w-3/4 mb-2 bg-blue-200 rounded-lg" />
+        <div className="animate-pulse h-4 w-2/3 bg-blue-200 rounded-lg" />
+      </div>
+      <div className="bg-blue-100 p-4 mb-2 border border-blue-400 rounded-lg shadow-md">
+        <div className="animate-pulse h-6 w-1/2 mb-2 bg-blue-200 rounded-lg" />
+        <div className="animate-pulse h-6 w-1/4 mb-2 bg-blue-200 rounded-lg" />
+        <div className="animate-pulse h-4 w-1/2 mb-2 bg-blue-200 rounded-lg" />
+        <div className="animate-pulse h-4 w-3/4 mb-2 bg-blue-200 rounded-lg" />
+        <div className="animate-pulse h-4 w-2/3 bg-blue-200 rounded-lg" />
+      </div>
+      <div className="bg-blue-100 p-4 mb-2 border border-blue-400 rounded-lg shadow-md">
+        <div className="animate-pulse h-6 w-1/2 mb-2 bg-blue-200 rounded-lg" />
+        <div className="animate-pulse h-6 w-1/4 mb-2 bg-blue-200 rounded-lg" />
+        <div className="animate-pulse h-4 w-1/2 mb-2 bg-blue-200 rounded-lg" />
+        <div className="animate-pulse h-4 w-3/4 mb-2 bg-blue-200 rounded-lg" />
+        <div className="animate-pulse h-4 w-2/3 bg-blue-200 rounded-lg" />
+      </div>
+      <div className="bg-blue-100 p-4 mb-2 border border-blue-400 rounded-lg shadow-md">
+        <div className="animate-pulse h-6 w-1/2 mb-2 bg-blue-200 rounded-lg" />
+        <div className="animate-pulse h-6 w-1/4 mb-2 bg-blue-200 rounded-lg" />
+        <div className="animate-pulse h-4 w-1/2 mb-2 bg-blue-200 rounded-lg" />
+        <div className="animate-pulse h-4 w-3/4 mb-2 bg-blue-200 rounded-lg" />
+        <div className="animate-pulse h-4 w-2/3 bg-blue-200 rounded-lg" />
+      </div>
+    </>
+  );
+};
+
+export default JobCardSkeleton;
--- src/pages/Dashboard/Freelancer/Dashboard/mainDashboard.jsx
+++ src/pages/Dashboard/Freelancer/Dashboard/mainDashboard.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import JobCardSkeleton from "./JobCardSkeleton";
 
 const MainDashboard = () => {
   const userObjectString = localStorage.getItem("user");
@@ -9,15 +10,15 @@ const MainDashboard = () => {
   const token = userObject.token;
 
   const [jobs, setJobs] = useState([]);
-  const [filter, setFilter] = useState({
-    budget: "",
-    duration: "",
-    skills: "",
-    search: "",
-  });
+  const [visibleJobs, setVisibleJobs] = useState(15);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [filteredJobs, setFilteredJobs] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [activeTab, setActiveTab] = useState("all");
 
   const fetchJobs = async () => {
     try {
+      setLoading(true);
       const response = await axios.get(
         `https://assist-api-okgk.onrender.com/api/v1/jobs`,
         {
@@ -27,7 +28,6 @@ const MainDashboard = () => {
         }
       );
 
-      // Sort jobs by creation date from newest to oldest
       const sortedJobs = response.data.sort((a, b) => {
         const dateA = new Date(a.createdAt);
         const dateB = new Date(b.createdAt);
@@ -35,23 +35,12 @@ const MainDashboard = () => {
       });
 
       setJobs(sortedJobs);
-      console.log(sortedJobs);
+      setFilteredJobs(sortedJobs);
+      setLoading(false);
     } catch (error) {
       console.error("Failed to fetch user data:", error);
+      setLoading(false);
     }
-  };
-
-  const handleFilterChange = (e) => {
-    const { name, value } = e.target;
-    setFilter((prevFilter) => ({
-      ...prevFilter,
-      [name]: value,
-    }));
-  };
-
-  const handleFilterSubmit = (e) => {
-    e.preventDefault();
-    fetchJobs();
   };
 
   useEffect(() => {
@@ -60,76 +49,160 @@ const MainDashboard = () => {
     }
   }, [userId, token]);
 
+  const loadMoreJobs = () => {
+    setVisibleJobs((prevVisibleJobs) => prevVisibleJobs + 10);
+  };
+
+  const handleSearch = (e) => {
+    const query = e.target.value.toLowerCase();
+    setSearchQuery(query);
+
+    const filtered = jobs.filter((job) => {
+      return (
+        job.title.toLowerCase().includes(query) ||
+        job.skills.join(", ").toLowerCase().includes(query) ||
+        job.budget.toString().includes(query) ||
+        job.duration.toString().includes(query)
+      );
+    });
+
+    setFilteredJobs(filtered);
+  };
+
+  const formatTimeAgo = (createdAt) => {
+    const currentTime = new Date();
+    const jobTime = new Date(createdAt);
+
+    const timeDifference = currentTime - jobTime;
+
+    const minute = 60 * 1000;
+    const hour = 60 * minute;
+    const day = 24 * hour;
+    const week = 7 * day;
+    const month = 30 * day;
+    const year = 365 * day;
+
+    if (timeDifference < minute) {
+      return "1 second ago";
+    } else if (timeDifference < hour) {
+      const minutes = Math.floor(timeDifference / minute);
+      return `${minutes} ${minutes === 1 ? "minute" : "minutes"} ago`;
+    } else if (timeDifference < day) {
+      const hours = Math.floor(timeDifference / hour);
+      return `${hours} ${hours === 1 ? "hour" : "hours"} ago`;
+    } else if (timeDifference < week) {
+      const days = Math.floor(timeDifference / day);
+      return `${days} ${days === 1 ? "day" : "days"} ago`;
+    } else if (timeDifference < month) {
+      const weeks = Math.floor(timeDifference / week);
+      return `${weeks} ${weeks === 1 ? "week" : "weeks"} ago`;
+    } else if (timeDifference < year) {
+      const months = Math.floor(timeDifference / month);
+      return `${months} ${months === 1 ? "month" : "months"} ago`;
+    } else {
+      const years = Math.floor(timeDifference / year);
+      return `${years} ${years === 1 ? "year" : "years"} ago`;
+    }
+  };
+
+  const filterJobs = (tab) => {
+    if (tab === "recommended") {
+      // Filter jobs for the "Recommended" tab based on your criteria
+      // Update the filteredJobs state accordingly
+    } else if (tab === "bestmatch") {
+      // Filter jobs for the "Best Match" tab based on your criteria
+      // Update the filteredJobs state accordingly
+    } else {
+      setFilteredJobs(jobs);
+    }
+    setActiveTab(tab);
+  };
+
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Job Listings</h1>
-      <form onSubmit={handleFilterSubmit} className="mb-4">
-        <div className="grid grid-cols-2 gap-4">
-          <div>
-            <label htmlFor="budget">Budget:</label>
-            <input
-              type="text"
-              id="budget"
-              name="budget"
-              value={filter.budget}
-              onChange={handleFilterChange}
-              className="border border-gray-300 rounded p-2 w-full"
-            />
-          </div>
-          <div>
-            <label htmlFor="duration">Duration:</label>
-            <input
-              type="text"
-              id="duration"
-              name="duration"
-              value={filter.duration}
-              onChange={handleFilterChange}
-              className="border border-gray-300 rounded p-2 w-full"
-            />
-          </div>
-          <div>
-            <label htmlFor="skills">Skills:</label>
-            <input
-              type="text"
-              id="skills"
-              name="skills"
-              value={filter.skills}
-              onChange={handleFilterChange}
-              className="border border-gray-300 rounded p-2 w-full"
-            />
-          </div>
-          <div>
-            <label htmlFor="search">Search:</label>
-            <input
-              type="text"
-              id="search"
-              name="search"
-              value={filter.search}
-              onChange={handleFilterChange}
-              className="border border-gray-300 rounded p-2 w-full"
-            />
-          </div>
-        </div>
+    <div className="container max-w-4xl mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4 text-center">
+        Available Projects
+      </h1>
+
+      <div className="flex justify-center my-4 space-x-4">
         <button
-          type="submit"
-          className="bg-blue-500 text-white py-2 px-4 mt-2 rounded hover-bg-blue-600"
+          className={`${
+            activeTab === "all" ? "bg-blue-500" : "bg-gray-300"
+          } text-gray-800 py-2 px-4 rounded hover:bg-blue-600`}
+          onClick={() => filterJobs("all")}
         >
-          Search
+          All Jobs
         </button>
-      </form>
-      <div className="grid grid-cols-1 gap-4">
-        {jobs.map((job) => (
-          <Link to={`/dashboard/job/${job._id}`} key={job._id}>
-            <div className="border border-gray-300 rounded p-4">
-              <h2 className="text-xl font-semibold">{job.title}</h2>
-              <p>Budget: {job.budget}</p>
-              <p>Duration: {job.duration}</p>
-              <p>Skills required: {job.skills.join(", ")}</p>
-              <p>{job.description}</p>
-            </div>
-          </Link>
-        ))}
+        <button
+          className={`${
+            activeTab === "recommended" ? "bg-blue-500" : "bg-gray-300"
+          } text-gray-800 py-2 px-4 rounded hover:bg-blue-600`}
+          onClick={() => filterJobs("recommended")}
+        >
+          Recommended
+        </button>
+        <button
+          className={`${
+            activeTab === "bestmatch" ? "bg-blue-500" : "bg-gray-300"
+          } text-gray-800 py-2 px-4 rounded hover:bg-blue-600`}
+          onClick={() => filterJobs("bestmatch")}
+        >
+          Best Match
+        </button>
       </div>
+
+      <input
+        type="text"
+        placeholder="Search projects using title, skills, duration, or budget"
+        onChange={handleSearch}
+        className="p-2 w-full border-2 border-blue-700 rounded-lg"
+      />
+
+      <hr className="my-2" />
+      {loading ? (
+        <JobCardSkeleton />
+      ) : (
+        <div className="grid grid-cols-1 gap-2">
+          {filteredJobs.slice(0, visibleJobs).map((job) => (
+            <Link to={`/dashboard/job/${job._id}`} key={job._id}>
+              <div className="bg-blue-100 p-4 mb-2 border border-blue-400 rounded-lg shadow-md">
+                <div className="flex justify-between">
+                  <h3 className="text-xl font-semibold">{job.title}</h3>
+                  <span className="text-gray-700">
+                    Posted: {formatTimeAgo(job.createdAt)}
+                  </span>
+                </div>
+                <div className="flex justify-start gap-4 items-center">
+                  <span className="text-blue-600 hover:underline">
+                    Budget: Ksh.{job.budget}
+                  </span>
+                  <span className="text-blue-600 hover:underline">
+                    Duration: {job.duration} days
+                  </span>
+                </div>
+                <p className="text-gray-600 my-2">{job.description}</p>
+                <div className="mt-4">
+                  <p className="text-gray-700">
+                    Skills: {job.skills.join(", ")}
+                  </p>
+                  <p className="text-gray-700">Bids: {job.bids.length}</p>
+                </div>
+              </div>
+            </Link>
+          ))}
+        </div>
+      )}
+
+      {visibleJobs < filteredJobs.length && (
+        <div className="grid place-items-center">
+          <button
+            onClick={loadMoreJobs}
+            className="bg-blue-500 text-center text-white py-2 px-4 mt-4 rounded hover:bg-blue-600"
+          >
+            Load More...
+          </button>
+        </div>
+      )}
     </div>
   );
 };
