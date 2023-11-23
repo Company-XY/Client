305c05b9fd17e903d695f59b83c418db8c048785
Olosko23 <oloogeorge633@gmail.com>
2023-11-23 18:27:07 +0300

    'profile'

--- src/pages/Dashboard/Client/Profile/getProfile.jsx
+++ src/pages/Dashboard/Client/Profile/getProfile.jsx
@@ -6,13 +6,11 @@ import { BiMoney } from "react-icons/bi";
 
 const Profile = () => {
   const [userData, setUserData] = useState(null);
-
   const userObjectString = localStorage.getItem("user");
-
   const userObject = JSON.parse(userObjectString);
-
   const userId = userObject._id;
   const token = userObject.token;
+  const profileCompletion = 60;
 
   const fetchUserData = async () => {
     try {
@@ -68,6 +66,21 @@ const Profile = () => {
     return "Unknown";
   };
 
+  const getGreeting = () => {
+    const currentTime = new Date().getHours();
+    let greeting = "";
+
+    if (currentTime >= 5 && currentTime < 12) {
+      greeting = "Good morning!";
+    } else if (currentTime >= 12 && currentTime < 18) {
+      greeting = "Good afternoon!";
+    } else {
+      greeting = "Good evening!";
+    }
+
+    return greeting;
+  };
+
   useEffect(() => {
     if (userId && token) {
       fetchUserData();
@@ -75,72 +88,77 @@ const Profile = () => {
   }, [userId, token]);
 
   return (
-    <div className="bg-snow-300 text-gray-800 p-4 flex flex-col md:flex-row">
+    <div className="flex flex-col w-full">
       {userData ? (
-        <div className="w-full basis-1/3">
-          <div className="w-52 mx-auto">
-            <div className="w-3/4 h-3/4">
-              <img
-                className="w-36 h-36 rounded-full"
-                src={userData.avatar.imageUrl}
-                alt="Profile"
-              />
+        <div id="Client_Profile_Card" className="rounded-lg">
+          <section className="p-4 mb-1">
+            <h2 className="text-2xl font-semibold px-9">
+              <span>{getGreeting()} </span>
+              <span>{userData.name}</span>
+            </h2>
+          </section>
+          <section className="flex">
+            <div className="hidden basis-1/4 md:flex flex-col px-4">
+              <div className="w-3/4 h-2/3 grid place-items-center mx-auto my-1 py-2">
+                <img
+                  className="w-32 h-32 rounded-full"
+                  src={userData.avatar.imageUrl}
+                  alt="Profile"
+                />
+              </div>
+              <div className="grid place-items-center mt-1">
+                <div className="flex space-x-2">
+                  <span className="">
+                    <IoLocation size={20} />
+                  </span>
+                  <span>{userData.location}</span>
+                </div>
+              </div>
+              <div className="text-center mt-1">
+                <span className="">Joined </span>
+                {calculateMemberDuration()}
+              </div>
             </div>
-          </div>
-          <div className="text-center md:text-center mt-4">
-            <h2 className="flex flex-row justify-center space-x-2">
-              <span className="grid place-items-center">
-                <IoLocation size={20} />
-              </span>
-              <span>{userData.location}</span>
-            </h2>
-            <h2 className="flex flex-row justify-center space-x-2">
-              <span className="grid place-items-center">
-                <AiOutlineMail size={20} />
-              </span>
-              <span>{userData.email}</span>
-            </h2>
-            <h2 className="flex flex-row justify-center space-x-2">
-              <span className="grid place-items-center">
-                <AiFillPhone size={20} />
-              </span>
-              <span>{userData.phone}</span>
-            </h2>
-            <h2 className="flex flex-row justify-center space-x-2">
-              <span className="grid place-items-center">
-                <BiMoney size={20} />
-              </span>
-              <span>Ksh. {userData.currentBalance}</span>
-            </h2>
-            <h2>
-              <span className="font-semibold">Joined </span>
-              {calculateMemberDuration()}
-            </h2>
-          </div>
+            <div className="basis-3/4">
+              <div className="flex space-x-5 my-1">
+                <div className="flex space-x-3">
+                  <span className="">
+                    <AiOutlineMail size={20} />
+                  </span>
+                  <span className="font-semibold">{userData.email}</span>
+                </div>
+                <div className="flex space-x-3">
+                  <span className="">
+                    <AiFillPhone size={20} />
+                  </span>
+                  <span className="font-semibold">{userData.phone}</span>
+                </div>
+              </div>
+              <div className="flex space-x-5 my-1">
+                <div className="flex space-x-3">
+                  <span className="">
+                    <BiMoney size={20} />
+                  </span>
+                  <span className="font-semibold">
+                    Ksh. {userData.currentBalance}
+                  </span>
+                </div>
+                <div className="flex">
+                  <span className="">Escrow Balance: </span>
+                  <span className="font-semibold">
+                    Ksh. {userData.escrowBalance}
+                  </span>
+                </div>
+              </div>
+              <div className="my-3 p-1">{userData.bio}</div>
+              <div className="">Rating: {userData.rating}</div>
+              <div className="cursor-pointer underline">Complete Profile</div>
+            </div>
+          </section>
         </div>
       ) : (
-        <div className="w-full text-center">
-          <p>Loading user profile...</p>
-        </div>
+        <div className="animate-spin rounded-full h-16 w-16 text-center grid place-items-center border-t-2 border-b-2 border-blue-500"></div>
       )}
-
-      <div className="w-full border-gray-300 p-4 basis-2/3">
-        {userData ? (
-          <div>
-            <h2 className="text-4xl font-semibold">{userData.name}</h2>
-            <h2 className="text-xl font-semibold mt-4">{userData.bio}</h2>
-            <h2 className="text-lg font-normal mt-4">
-              Rating: {userData.rating}
-            </h2>
-          </div>
-        ) : (
-          <div>
-            <h2 className="text-4xl font-semibold">Loading...</h2>
-            <h2 className="text-xl font-semibold mt-4">Loading...</h2>
-            <h2 className="text-lg font-normal mt-4">Rating: Loading...</h2>
-          </div>
-        )}
-      </div>
     </div>
   );
 };
