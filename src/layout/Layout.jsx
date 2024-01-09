b69c05fa9608b59836a1f8fc9fe61d7543748e01
Olosko23 <59167901+Olosko23@users.noreply.github.com>
2024-01-09 08:49:34 +0300

    Update Layout.jsx

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -2,12 +2,14 @@ import { useSelector } from "react-redux";
 import Header from "../components/Header/NewHeader";
 import Routers from "../routes/Routers";
 import Footer2 from "../components/Footer/Footer2";
+import Scroll from "../components/ScrollToTopOnPageChange";
 
 const Layout = () => {
   const darkMode = useSelector((state) => state.darkMode.darkMode);
 
   return (
     <>
+    <Scroll />
       <section className="w-full fixed top-0">
         <Header darkMode={darkMode} />
       </section>
